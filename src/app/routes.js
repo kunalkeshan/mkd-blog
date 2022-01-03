a55976188d6cc0182e0174cc1ee9956cf7cca627
Kunal Keshan <kunalkeshan12@gmail.com>
2022-01-03 23:05:56 +0530

    Main File Structure Template Set

--- src/app/routes.js
+++
@@ -1,9 +0,0 @@
-const Router = require("express").Router();
-
-const userRouter = require("./user");
-const commentsRouter = require("./comments");
-
-Router.use(userRouter);
-Router.use(commentsRouter);
-
-module.exports = Router;
